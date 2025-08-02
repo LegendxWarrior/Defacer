b7767cadcf0456e2f4ad0bd306109b764b663d86
LegendxWarrior <warriorlegendx999@gmail.com>
2025-08-02 17:00:02 +0530

    Update defacer.js

--- defacer.js
+++ defacer.js
@@ -2,10 +2,8 @@ document.querySelector("html").innerHTML = `
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Marmelad&display=swap" rel="stylesheet">
   <title>Hacked by Legend</title>
+  <link href="https://fonts.googleapis.com/css2?family=Marmelad&display=swap" rel="stylesheet">
   <style>
     * {
       margin: 0;
@@ -13,143 +11,109 @@ document.querySelector("html").innerHTML = `
       box-sizing: border-box;
     }
 
-    html, body {
-      height: 100%;
-      width: 100%;
+    body {
       background: #000;
+      color: #fff;
       font-family: 'Marmelad', sans-serif;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      padding: 5vh 5vw;
-      color: white;
+      padding: 5vh 2vw;
       text-align: center;
     }
 
     .Main {
+      font-size: 6vw;
       color: #FF0000;
-      font-size: 3.5vw;
       animation: main-anim 15s infinite;
     }
 
     .wish {
+      font-size: 8vw;
       color: #FFF2F2;
-      font-size: 6vw;
       margin-top: 4vh;
       animation: wish-anim 10s infinite;
     }
 
     img {
-      margin-top: 5vh;
-      max-width: 90%;
+      width: 80%;
+      max-width: 500px;
       height: auto;
+      margin-top: 5vh;
     }
 
     p {
-  font-size: 5.5vw;
-  margin-top: 4vh;
-  background: linear-gradient(180deg, red, white, green);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-clip: text;
-  color: transparent;
-}
-
-@media (min-width: 768px) {
-p {
-  font-size: 1.5rem;
-}
-.show, .about {
-  font-size: 1.2rem;
-}
-}
-
-    ..show {
-  margin-top: 5vh;
-  font-size: 3.5vw; /* was 4vw */
-  color: red;
-}
-
-.about {
-  color: #FF8F04;
-  font-size: 3.8vw; /* was 4.5vw */
-  margin-top: auto;
-}
-
-    @keyframes main-anim {
-      0% {
-        text-shadow: none;
-      }
-      50% {
-        text-shadow:
-          -2px -2px 0px #000000,
-          2px 2px 0px #000000,
-          2px -2px 0px #000,
-          -2px 2px 0px #000,
-          15px 15px 15px #FF0000,
-          -15px 15px 15px #FF0000,
-          15px -15px 20px #FF0000,
-          -15px -15px 30px #FF0000;
-      }
+      font-size: 4vw;
+      max-width: 800px;
+      margin: 5vh auto 0;
+      background: linear-gradient(180deg, red, white, green);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      background-clip: text;
+      color: transparent;
     }
 
-    @keyframes wish-anim {
-      0% {
-        text-shadow: 0px;
-      }
-      30% {
-        color: #00FF01;
-        text-shadow:
-          -6px -6px 0px #000000,
-          -2px 2px 0px #000000,
-          2px -2px 0px #000000,
-          2px 2px 0px #000000,
-          15px 15px 15px #FF0000,
-          -15px 15px 15px #FF0000,
-          15px -15px 20px #FF4600,
-          -15px -15px 30px #FF4600;
-      }
-      50% {
-        text-shadow: none;
-      }
-      80% {
-        color: #FF5000;
-        text-shadow:
-          -2px -2px 0px #000000,
-          -2px 2px 0px #000000,
-          2px -2px 0px #000000,
-          6px 4px 0px #000000,
-          15px 15px 15px #00ff3c,
-          -15px 15px 15px #00FF3C,
-          15px -15px 20px #00ff3c,
-          -15px -15px 30px #00ff3c;
-      }
-      100% {
-        text-shadow: 0px;
-      }
+    .show {
+      margin-top: 5vh;
+      font-size: 2.5vw;
+    }
+
+    .show h2 {
+      color: red;
+    }
+
+    .about {
+      font-size: 2.5vw;
+      color: #FF8F04;
+      margin-top: 6vh;
     }
 
     @media (min-width: 768px) {
       .Main {
-        font-size: 2.5rem;
+        font-size: 3rem;
       }
 
       .wish {
-        font-size: 3rem;
+        font-size: 4rem;
       }
 
       p, .show, .about {
         font-size: 1.2rem;
       }
     }
+
+    @keyframes main-anim {
+      0% { text-shadow: none; }
+      50% {
+        text-shadow: -2px -2px 0 #000, 2px 2px 0 #000,
+                     15px 15px 15px #FF0000, -15px 15px 15px #FF0000,
+                     15px -15px 20px #FF0000, -15px -15px 30px #FF0000;
+      }
+    }
+
+    @keyframes wish-anim {
+      0% { text-shadow: none; }
+      30% {
+        color: #00FF01;
+        text-shadow: -6px -6px 0 #000, -2px 2px 0 #000, 2px -2px 0 #000,
+                     15px 15px 15px #FF0000, -15px 15px 15px #FF0000,
+                     15px -15px 20px #FF4600, -15px -15px 30px #FF4600;
+      }
+      50% { text-shadow: none; }
+      80% {
+        color: #FF5000;
+        text-shadow: -2px -2px 0 #000, 6px 4px 0 #000,
+                     15px 15px 15px #00ff3c, -15px 15px 15px #00FF3C,
+                     15px -15px 20px #00ff3c, -15px -15px 30px #00ff3c;
+      }
+      100% { text-shadow: none; }
+    }
   </style>
 </head>
 <body>
   <h1 class="Main">[+] Hacked by LegendxWarrior [+]</h1>
   <h1 class="wish"><strong>[ Happy Independence Day ]</strong></h1>
-  <img src="https://raw.githubusercontent.com/legendxWarrior/Defacer/main/flag.jpg" alt="Flag">
-  <p>I find vulnerability and just compromised your system. You need to know more about security because you are a baby in cyber security. I laugh on your security that is compromised by a beginner hacker. I am an Indian and I just hacked your website to wish the Independence Day of India.</p>
+  <img src="https://raw.githubusercontent.com/legendxWarrior/Defacer/main/flag.jpg" alt="Indian Flag">
+  <p>
+    I find vulnerability and just compromised your system. You need to know more about security because you are a baby in cyber security. I laugh on your security that is compromised by a beginner hacker. I am an Indian and I just hacked your website to wish the Independence Day of India.
+  </p>
   <div class="show">
     <h2>{ धर्मो रक्षति रक्षितः }<br>|| भारत माता की जय ||<br>🚩जय श्री राम 🚩<br>⚔️⚔️⚔️</h2>
   </div>
